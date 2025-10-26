df1e41ebe49585cffd7b3ff47418fde275bdf53c
mariamanda <amanda.morais42@hotmail.com>
2025-10-26 15:45:42 -0300

    feat: adiciona permissões especificas para o admin

--- app/inventory/views.py
+++ app/inventory/views.py
@@ -1,7 +1,16 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 from core.models import AutoPart
 from inventory import serializers
+
+class OnlyAdminPermissions(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        
+        return request.user and request.user.is_staff
+    
 
 class AutoPartView(viewsets.ModelViewSet):
     serializer_class = serializers.AutoPartSerializer
     queryset = AutoPart.objects.all()
+    permission_classes = [OnlyAdminPermissions]
