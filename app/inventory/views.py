93e87bd49d965fc65a0f001fcf4c0bda5f1991ec
mariamanda <amanda.morais42@hotmail.com>
2025-10-26 19:14:54 -0300

    fix: corrige erro que permitia usuários não autenticados listar e detalhar objetos

--- app/inventory/views.py
+++ app/inventory/views.py
@@ -2,15 +2,15 @@ from rest_framework import viewsets, permissions
 from core.models import AutoPart
 from inventory import serializers
 
-class OnlyAdminPermissions(permissions.BasePermission):
-    def has_permission(self, request, view):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        
-        return request.user and request.user.is_staff
-    
 
 class AutoPartView(viewsets.ModelViewSet):
     serializer_class = serializers.AutoPartSerializer
     queryset = AutoPart.objects.all()
-    permission_classes = [OnlyAdminPermissions]
+
+    def get_permissions(self):
+        if self.action in ['list', 'retrieve']:
+            permission_classes = [permissions.IsAuthenticated]
+        else:
+            permission_classes = [permissions.IsAdminUser]
+
+        return [permission() for permission in permission_classes]
